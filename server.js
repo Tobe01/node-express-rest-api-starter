c8b4ce45e27ee07cb9c28c88d80e52139c1042fc
Tobechi Duru <tobechi.duru001@gmail.com>
2025-08-13 23:05:10 -0700

    Ne wupdates

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(express.json());
 const tasksRouter = require('./routes/tasks');
 app.use('/tasks', tasksRouter);
 
+// Root route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Simple Node.js and Express REST API',
+    endpoints: {
+      getTasks: '/tasks',
+      addTask: 'POST /tasks',
+      deleteTask: 'DELETE /tasks/:id'
+    }
+  });
+});
+
 // Start server
 app.listen(3000, () => {
   console.log('Server running on port 3000');
